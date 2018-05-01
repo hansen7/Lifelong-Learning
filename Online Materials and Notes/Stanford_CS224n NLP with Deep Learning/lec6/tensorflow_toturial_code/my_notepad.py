e466b47b800f2a71fb283487eedae2575f3167c4
Hansen Wang <hc.wang96@gmail.com>
2018-05-01 12:18:28 +0800

    TensorFlow Tutorial

---
+++ Online Materials and Notes/Stanford_CS224n NLP with Deep Learning/lec6/tensorflow_toturial_code/my_notepad.py
@@ -0,0 +1,7 @@
+#It is just a simple process to run neural network udner TensorFlow
+import numpy as py
+import tensorflow as tf
+import matplotlib
+matplotlib.use('TKagg')
+from matplotlib import pyplot as plt
+
